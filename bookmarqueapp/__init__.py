803539d2b3905bcd54f50fd2c1c41396f072eb1e
Nick Real <nsr70583@uga.edu>
2021-03-04 21:46:33 -0500

    Created the profile and edit profile pages, and updated the css for these pages

--- bookmarqueapp/__init__.py
+++ bookmarqueapp/__init__.py
@@ -54,3 +54,19 @@ def manageBooks():
 @app.route('/manage-books/book-entry')
 def bookEntry():
     return render_template('book_entry.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/profile/update-password')
+def password_panel():
+    return render_template('update_password.html')
+
+@app.route('/profile/update-card')
+def card_panel():
+    return render_template('update_card.html')
+
+@app.route('/profile/edit')
+def edit_profile():
+    return render_template('edit_profile.html')
