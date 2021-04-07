20d44816535b385cfaa9e93b90214e6b40197797
Nick Real <nsr70583@uga.edu>
2021-04-07 19:46:30 -0400

    initial add book functionality

--- bookmarqueapp/__init__.py
+++ bookmarqueapp/__init__.py
@@ -91,9 +91,40 @@ def admin():
 def managePromotions():
     return render_template('manage_promotions.html')
 
-@app.route('/manage-books')
+@app.route('/manage-books', methods = ['POST', 'GET'])
 def manageBooks():
     cursor = mysql.connection.cursor()
+    if request.method == 'POST':
+        ISBN = request.form.get('ISBN')
+
+        bookTitle = request.form.get('title')
+
+        author = request.form.get('author')
+        if (author != None):
+            author = author.split();
+        
+        subject = request.form.get('subject')
+        if (subject != None):
+            if (',' in subject):
+                subject = subject.split(',')
+                for x in subject:
+                    cursor.execute('''SELECT categoryID FROM book_categories WHERE categoryName = %s;''', [x]);
+                    x = cursor.fetchone()
+                    x = x[0]
+                    cursor.execute('''INSERT INTO book_has_book_categories (ISBN, categoryID) VALUES (%s, %s);''', (ISBN, [x]))
+            else:
+                cursor.execute('''SELECT categoryID FROM book_categories WHERE categoryName = %s;''', [subject]);
+                subject = cursor.fetchone()
+                subject = subject[0]
+                cursor.execute('''INSERT INTO book_has_book_categories (ISBN, categoryID) VALUES (%s, %s);''', (ISBN, [subject]))
+        
+        copies = request.form.get('copies')
+
+        cursor.execute('''INSERT INTO book (ISBN, bookTitle, authorFName, authorLName, bookQuantity) VALUES (%s, %s, %s, %s, %s);''', (ISBN, bookTitle, author[0], author[1], [copies]))
+        
+        mysql.connection.commit()
+        return redirect(url_for('manageBooks'))
+        
     cursor.execute('''SELECT book.ISBN, bookTitle, CONCAT(authorFName, " ", authorLName) AS authorName, group_concat(categoryName) AS categories, bookQuantity FROM book JOIN book_has_book_categories ON book.ISBN = book_has_book_categories.ISBN JOIN book_categories ON book_has_book_categories.categoryID = book_categories.categoryID GROUP BY ISBN;''')
     bookData = cursor.fetchall()
     mysql.connection.commit()
